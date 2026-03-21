27d87d32e1f97d106548e8e02293b30a7bb273d2
yao-yu1999 <yao_yu1999@163.com>
2026-03-21 10:21:01 +0800

    二分查找

--- 数组/35搜索插入位置.py
+++ 数组/35搜索插入位置.py
@@ -1,5 +1,6 @@
-# https://leetcode.cn/problems/binary-search/description/
-# 通用写法:
+# https://leetcode.cn/problems/search-insert-position/
+# 这题也是不重复的升序数组。思路：返回 nums 中的第一个（最左边的）大于或等于 target 的数的下标。
+
 # lower_bound 返回最小的满足 nums[i] >= target 的 i
 # 如果“数组为空”或者“所有数都 < target”，则返回 len(nums)；要求 nums 是非递减的，即 nums[i] <= nums[i + 1]
 
@@ -7,7 +8,7 @@
 # nums[left-1] < target
 # nums[right+1] >= target
 
-
+# 通用写法:
 from typing import List
 
 # 闭区间写法
@@ -43,29 +44,11 @@ def lower_bound3(nums: List[int], target: int) -> int:
             right = mid  # 范围缩小到 (left, mid)
     return right  # 或者 left+1
 
+
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        i = lower_bound(nums, target)
-        return i if i < len(nums) and nums[i] == target else -1  # 如果不存在需要返回-1
-        
-
-'''
-这个仅适用于二分查找中不重复升序数组【不建议】
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)-1 # 定义下标范围
-        while left<=right:  # 循环条件是升序数组
-            mid = (right - left)//2 + left  # 先定位中间数的位置（下标）
-
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1 # 说明在右半区间，将左边界移动到中间+1
-            else:
-                right = mid - 1 # 说明在左半区间，将右边界移动到中间-1
-        return -1 # 考虑target不存在的情况
-'''
-
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i = lower_bound(nums, target)  # 直接插入
+        return i
 
 # 测试（可选）
 if __name__ == "__main__":
