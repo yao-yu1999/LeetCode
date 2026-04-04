568ac217bf9bc6d8754e200b3a6335a430f31fb2
yao-yu1999 <yao_yu1999@163.com>
2026-04-04 09:29:12 +0800

    8-二叉树

---
+++ 8二叉树/543二叉树的直径.py
@@ -0,0 +1,31 @@
+# https://leetcode.cn/problems/diameter-of-binary-tree/?envType=study-plan-v2&envId=top-100-liked
+
+from typing import Optional
+from polars import TreeNode
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        max_len = 0  # 记录全局最大直径
+        
+        # 嵌套递归
+        def dfs(node: Optional[TreeNode]) -> int:
+            if node is None: return -1 # 如当前节点为空，说明这条链不存在，定义链长为 -1，方便后面 +1 后正确算出叶子节点的链长：-1 +1 = 0
+                
+            l_len = dfs(node.left) + 1  # 左链长 = 左子树的最大链长 + 1（当前节点连左子节点的一条边）
+            r_len = dfs(node.right) + 1 # 右链长 = 右子树的最大链长 + 1（当前节点连右子节点的一条边）
+
+            nonlocal max_len # 在内部函数中修改外部变量，必须声明 nonlocal
+            max_len = max(max_len, l_len + r_len) # 当前节点的直径 = 左链长 + 右链长，更新全局最大直径
+
+            return max(l_len, r_len) # 返回给父节点只能选一条链
+
+        dfs(root) # 开始递归
+        
+        return max_len # 返回全局最大直径
